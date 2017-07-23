079abbb356f544d00a9da410d4663a7ced4c49fb
WayneSiebenaler <wayne.siebenaler@pcc.edu>
2017-07-23 16:42:00 -0700

    added main method that prints hello world

--- src/edu/pcc/cis/helloworld2/HelloWorld2.java
+++ src/edu/pcc/cis/helloworld2/HelloWorld2.java
@@ -4,4 +4,7 @@ package edu.pcc.cis.helloworld2;
  * Created by Wayne Siebenaler on 7/22/2017
  */
 public class HelloWorld2 {
+    public static void main (String []args) {
+        System.out.println("Hello, World!");
+    }
 }

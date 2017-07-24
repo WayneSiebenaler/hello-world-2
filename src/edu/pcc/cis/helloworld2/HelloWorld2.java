25943260da31978fffcfb736d6af344dd38bd0b2
WayneSiebenaler <wayne.siebenaler@pcc.edu>
2017-07-23 17:08:21 -0700

    print date added to main method

--- src/edu/pcc/cis/helloworld2/HelloWorld2.java
+++ src/edu/pcc/cis/helloworld2/HelloWorld2.java
@@ -1,10 +1,11 @@
 package edu.pcc.cis.helloworld2;
-
+import java.util.Date;
 /**
  * Created by Wayne Siebenaler on 7/22/2017
  */
 public class HelloWorld2 {
     public static void main (String []args) {
         System.out.println("Hello, World!");
+        System.out.println(new Date());
     }
 }
